11af7b4d44a078718e6eadba591b238e4b2a37ca
anmyla <myla.ariaga@gmail.com>
2023-04-27 12:59:37 +0200

    filled initial deck with this method: initialDeck() in Class Deck

--- src/Classes/Deck.java
+++ src/Classes/Deck.java
@@ -20,13 +20,13 @@ public class Deck {
         for (int i = 0; i < colorValue.length-1; i++) { // 4 Repetitions ColorValue 0= R, 1 = G, 2 = B, 3 = Y
             cardDeck.add(new Cards(colorValue[i], faceValue[0], points[0])); // this adds the following cards: ( R0, G0, B0, Y0)
             for (int j = 1; j < faceValue.length-4; j++) {   // 11 repetitions
-                cardDeck.add( new Cards(faceValue[j], colorValue[i], points[j] )); // this adds the following cards: (1copy R1 - <->, G0 - <->, B0 - <->, Y0 - <->)
-                cardDeck.add( new Cards(faceValue[j], colorValue[i], points[j] )); // this adds the following cards: (2copy R1 - <->, G0 - <->, B0 - <->, Y0 - <->)
+                cardDeck.add( new Cards(faceValue[j], colorValue[i], points[j] )); // this adds the following cards: (1st copy R1 - <->, G1 - <->, B1 - <->, Y1 - <->)
+                cardDeck.add( new Cards(faceValue[j], colorValue[i], points[j] )); // this adds the following cards: (2nd copy R1 - <->, G1 - <->, B1 - <->, Y1 - <->)
             }
 
             for (int j = faceValue.length-4; j < faceValue.length-2; j++) { //2 repetitions
-                cardDeck.add( new Cards(faceValue[j], colorValue[i], points[points.length-2] )); //this adds the following cards: (1copy +2, +4)
-                cardDeck.add( new Cards(faceValue[j], colorValue[i], points[points.length-2] )); //this adds the following cards: (2copy +2, +4)
+                cardDeck.add( new Cards(faceValue[j], colorValue[i], points[points.length-2] )); //this adds the following cards: (1st copy +2, +4)
+                cardDeck.add( new Cards(faceValue[j], colorValue[i], points[points.length-2] )); //this adds the following cards: (2nd copy +2, +4)
             }
         }
 
